91ae2290b16b93ac780aa684da5c187ed1427a1a
shakibdshy <shakibdshy@gmail.com>
2022-04-05 22:07:13 +0600

    Review Update

--- src/hooks/useReview.js
+++ src/hooks/useReview.js
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, {
+  useEffect,
+  useState
+} from "react";
 
 const useReview = () => {
   const [reviews, setReviews] = useState([]);
-  
+
   useEffect(() => {
     fetch("testimonial.json")
       .then((response) => response.json())
       .then((json) => setReviews(json));
-  });
+  }, []);
   return [reviews, setReviews];
 }
 
