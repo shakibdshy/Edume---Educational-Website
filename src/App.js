38a18381dc74c5fde2d71a350f32407d4e27f7db
shakibdshy <shakibdshy@gmail.com>
2022-04-05 12:53:20 +0600

    1st Question asn done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Dashboard from './Components/Dashboard/Dashboard';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Blog from './Components/Blog/Blog';
-import Review from './Components/Review/Review';
 import NotFound from './Components/NotFound/NotFound';
 import ReviewTwo from './Components/ReviewTow/ReviewTwo';
 
@@ -16,8 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/blog" element={<Blog />} />
         <Route path="/reviewtwo" element={<ReviewTwo />} />
+        <Route path="/blog" element={<Blog />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import './Blog.css';
 
 const Blog = () => {
     return (
-        <div>
-            Blog Page
-        </div>
+        <section className='blog-area'>
+            <Container>
+                <Row>
+                    <Col md={12}>
+                        <h1>What is Context API?</h1>
+                        <p>Context API Is the New Feature In React App. Earlier React Version 16.3 React First Introduce Context API. We Know That how match we provides a props to declare our child component. It's Say "prop drilling" or moving props from parent child to child component. This is where we can slow down our application and cause development issues.</p>
+                    </Col>
+                    <Col md={12}></Col>
+                </Row>
+            </Container>
+        </section>
     );
 };
 
