ad6dde1eae0c5339181839306d23bac51c5f5165
shakibdshy <shakibdshy@gmail.com>
2022-04-05 13:18:54 +0600

    Question Done

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -11,7 +11,10 @@ const Blog = () => {
                         <h1>What is Context API?</h1>
                         <p>Context API Is the New Feature In React App. Earlier React Version 16.3 React First Introduce Context API. We Know That how match we provides a props to declare our child component. It's Say "prop drilling" or moving props from parent child to child component. This is where we can slow down our application and cause development issues.</p>
                     </Col>
-                    <Col md={12}></Col>
+                    <Col md={12}>
+                        <h2>What Is Semantic Tag</h2>
+                        <p>Semantic Tag or HTML elements are those that clearly describe their meaning in a Developer and Browser readable way. Semantic Tag such as <code>header</code>, <code>footer</code> and <code>article</code> are all considered semantic because they accurately describe the purpose of the element and the type of content that is inside them. When using semantic tags that meanings it's not only a presentation purposes, be careful that you don't use them incorrectly simply for their common display properties. According to W3C's HTML documentation: "A section is a thematic grouping of content, typically with a heading."</p>
+                    </Col>
                 </Row>
             </Container>
         </section>
